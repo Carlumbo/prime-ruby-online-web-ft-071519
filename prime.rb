2e01816a8e0e40869dcc2f92eb9214078a9c61cd
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-22 16:49:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(num)
   if num < 0 || num == 0 || num == 1
     return false 
   else 
-    (2..num-1).to_a.all? do |optimus|
+    (2..num).to_a.all? do |optimus|
       num % optimus != 0 
     end 
   end 
